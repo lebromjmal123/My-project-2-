54942d19d5a911a619622981aa18f5e3487793a3
YouKnowWho113 <168092669+YouKnowWho113@users.noreply.github.com>
2025-01-08 09:48:21 +0700

    sua dc hook

--- Assets/Script/Grappler.cs
+++ Assets/Script/Grappler.cs
@@ -1,82 +1,92 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
-    public Camera mainCamera;
-    public LineRenderer lineRenderer;
-    public SpringJoint2D springJoint;
-    public float springFrequency = 2f;
-    public float springDamping = 0.7f;
-    public float maxRopeLength = 15f;  // Maximum rope length
-    public float minRopeLength = 5f;   // Minimum rope length (when swinging)
-    public float swingLengthReductionSpeed = 0.1f; // Speed at which rope shortens when swinging
+    public Camera mainCamera;                
+    public LineRenderer lineRenderer;        
+    public SpringJoint2D springJoint;        
+    public float ropeLength = 5f;            
 
-    private Vector2 initialRopePosition;
+    private Vector2 ropeAnchorPoint;         
+    private Rigidbody2D playerRigidbody;     
 
     void Start()
     {
-        springJoint.enabled = false;
-        springJoint.frequency = springFrequency;
-        springJoint.dampingRatio = springDamping;
-        initialRopePosition = transform.position;  // Store initial position of the player
+        springJoint.enabled = false;         
+        playerRigidbody = GetComponent<Rigidbody2D>(); 
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            // Get the mouse position in world space
-            Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
-
-            // Set the starting position of the line
-            lineRenderer.SetPosition(0, mousePos);
-            lineRenderer.SetPosition(1, transform.position);
-
-            // Configure the spring joint
-            springJoint.connectedAnchor = mousePos;
-            springJoint.enabled = true;
-
-            // Set the initial distance for the rope when it is created
-            springJoint.distance = Vector2.Distance(transform.position, mousePos);
-
-            // Enable the line renderer
-            lineRenderer.enabled = true;
+            CreateRope();
         }
         else if (Input.GetKeyUp(KeyCode.E))
         {
-            // Disable the spring joint and line renderer
-            springJoint.enabled = false;
-            lineRenderer.enabled = false;
+            ReleaseRope();
         }
 
         if (springJoint.enabled)
         {
-            // Update the line renderer to follow the player's position
-            lineRenderer.SetPosition(1, transform.position);
+            UpdateRope();
+        }
+    }
 
-            // Limit the rope length to maxRopeLength when the player isn't swinging
-            if (Vector2.Distance(initialRopePosition, transform.position) > maxRopeLength)
-            {
-                springJoint.distance = maxRopeLength;
-            }
+    private void CreateRope()
+    {
+        
+        Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-            // Reduce the length of the rope while swinging (when moving)
-            if (springJoint.distance > minRopeLength && IsPlayerSwinging())
+        
+        Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+
+        
+        ropeAnchorPoint = (Vector2)transform.position + direction * ropeLength;
+
+        
+        springJoint.connectedAnchor = ropeAnchorPoint;
+        springJoint.distance = ropeLength; 
+        springJoint.enabled = true;
+
+        
+        lineRenderer.SetPosition(0, ropeAnchorPoint); 
+        lineRenderer.SetPosition(1, transform.position); 
+        lineRenderer.enabled = true;
+    }
+
+    private void ReleaseRope()
+    {
+        
+        springJoint.enabled = false;
+        lineRenderer.enabled = false;
+    }
+
+    private void UpdateRope()
+    {
+        
+        lineRenderer.SetPosition(0, ropeAnchorPoint); 
+        lineRenderer.SetPosition(1, transform.position); 
+    }
+
+    private void FixedUpdate()
+    {
+        if (springJoint.enabled)
+        {
+            
+            float currentDistance = Vector2.Distance(transform.position, ropeAnchorPoint);
+
+            
+            if (currentDistance > ropeLength)
             {
-                springJoint.distance -= swingLengthReductionSpeed * Time.deltaTime;
+                Vector2 direction = ((Vector2)transform.position - ropeAnchorPoint).normalized;
+                Vector2 newPosition = ropeAnchorPoint + direction * ropeLength;
+                playerRigidbody.MovePosition(newPosition);
             }
         }
     }
 
-    // Check if the player is swinging based on their velocity (or other factors like input)
-    bool IsPlayerSwinging()
-    {
-        // You can adjust this condition based on player movement or velocity
-        // Here we're checking if the player's horizontal velocity exceeds a threshold.
-        float velocityThreshold = 2f;  // Minimum velocity for swinging
-        return Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > velocityThreshold;
-    }
 }
 
